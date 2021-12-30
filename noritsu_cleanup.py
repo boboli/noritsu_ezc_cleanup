32bdda30d638f3043b0e43477115c17ef944557b
Bobo Li <azncastigar@gmail.com>
2021-12-30 00:54:23 -0800

    [feature] Added renaming files feature, but there's a bug
    If the Noritsu decided that multiple frames are named ###, they'll
    collide and only one image will survive the renaming... need to check
    for duplicate frame names and resolve the conflict.

--- noritsu_cleanup.py
+++ noritsu_cleanup.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 # assume folder structure:
 # 20211226/  <- date
@@ -13,8 +14,19 @@ from pathlib import Path
 
 
 class NoritsuEZCCleaner:
+    IMAGE_NAME_PATTERN = \
+        r"(?P<roll_number>\d{8})" \
+        r"(?P<frame_number>\d{4})" \
+        r"_" \
+        r"(?P<frame_name>.*)"
+    image_name_matcher = re.compile(IMAGE_NAME_PATTERN)
+
     def __init__(self):
         pass
+
+    def clean(self):
+        for image_dir in self.find_all_image_dirs():
+            self.rename_images(image_dir)
 
     def find_all_image_dirs(self):
         """
@@ -27,17 +39,41 @@ class NoritsuEZCCleaner:
         """
         return Path.cwd().glob("**/" + ("[0-9]" * 8))
 
-    def rename_images(self, images_dir, roll_padding=4, frame_padding=2):
+    def rename_images(self, images_dir, roll_padding=4):
         """
         Renames the images in the images_dir directory in the format:
             R{roll_number}F{frame_name}.jpg (or .tif)
 
         roll_padding is how many characters of zero padding to add for the
         roll number
-        frame_padding is how many characters of zero padding to add for the
-        frame name
         """
-        pass
+        roll_number = None
+        for image_path in images_dir.glob("*"):
+            filename = image_path.stem
+            prefix = image_path.parent
+            suffix = image_path.suffix
+
+            if suffix not in (".jpg", ".tif"):
+                continue
+
+            match = self.image_name_matcher.match(filename)
+            if not match:
+                raise ValueError(
+                    f"image filename doesn't match expected format: "
+                    f"{image_path}")
+            if not roll_number:
+                roll_number = match.group("roll_number")
+            elif roll_number != match.group("roll_number"):
+                raise ValueError(
+                    f"image filename doesn't match other files: {image_path}")
+            formatted_roll_number = f"{int(roll_number):0>{roll_padding}d}"
+
+            frame_name = match.group("frame_name")
+            new_filename = f"R{formatted_roll_number}F{frame_name}"
+
+            new_filepath = prefix.joinpath(f"{new_filename}{suffix}")
+            print(f"{filename} => {new_filename}")
+            # image_path.rename(new_filepath)
 
     def add_date_info(self, images_dir):
         """
@@ -48,3 +84,8 @@ class NoritsuEZCCleaner:
         (such as Lightroom).
         """
         pass
+
+
+if __name__ == "__main__":
+    cleaner = NoritsuEZCCleaner()
+    cleaner.clean()
